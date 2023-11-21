4f3104f13bb8056ba37c845b35a8a73a8017c074
SAHA-YJB <pkomom123@naver.com>
2023-11-21 13:30:05 +0900

    features: newMeetupPage init

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,10 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+
 //new-meetup/
+export default function NewMeetupPage() {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
