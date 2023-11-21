9d5b355c3886b444f00b92f73d3ca2d6f18d5c49
SAHA-YJB <pkomom123@naver.com>
2023-11-21 14:01:43 +0900

    features: moduleCss / detail meetup

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA4MTlfMTI4%2FMDAxNjkyNDMxODY0NDM2.0jBcFTPx99FFN9GC1MZ8PdFc2L9we3i8ponFSRi0z-4g.T54iqIDXTWmtLIayhNb_fCvAIuldGj431z2ORXNRdzwg.JPEG.senser111%2F1000005147.jpg&type=a340"
+      title="first"
+      address="123"
+      description="this is first"
+    />
+  );
+}
