a0cea32d6535a6f93fd19de5c1d33513ea9f740a
SAHA-YJB <pkomom123@naver.com>
2023-11-21 17:18:39 +0900

    test: test

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,20 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 //new-meetup/
 export default function NewMeetupPage() {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const res = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    router.push("/");
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
