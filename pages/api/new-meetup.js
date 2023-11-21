20c7abf612d8ba63733bff5bac06dd8a3899d9bd
SAHA-YJB <pkomom123@naver.com>
2023-11-21 16:57:47 +0900

    features: db connect practice

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,21 @@
+//리액트 코드 x
+//서버사이드 코드 o
+// api/new-meetup
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
